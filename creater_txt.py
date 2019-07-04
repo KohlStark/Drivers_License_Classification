2085efe61c4db95d7c83c5097d418f3b9ce7d580
Kohl Stark <45770597+KohlStark@users.noreply.github.com>
2019-07-03 23:33:50 -0700

    Python script to create two hundred .txt files
    This script creates two hundred .txt files labeled Drivers_License_#.txt which each hold the drivers' license details of the drivers' license matching photo.

---
+++ creater_txt.py
@@ -0,0 +1,19 @@
+
+
+
+for i in range(200):
+	print("hey there", i)
+	file = open("Drivers_License_" + str(i+1) + ".txt", "w") 
+	file.write("First Name:\n")
+	file.write("Last Name:\n")
+	file.write("Address:\n")
+	file.write("Sex:\n")
+	file.write("Height:\n")
+	file.write("Weight:\n")
+	file.write("Eye color:\n")
+	file.write("Hair Color:\n")
+	file.write("DOB:\n")
+	file.write("Donor Status:")
+	file.close()
+
+	 
